6ebd4291da26d5a97b0394b57d379984a0689559
Ealdrich3 <77710782+Ealdrich3@users.noreply.github.com>
2023-05-10 21:58:17 -0500

    finished to-do list program
    final product for my final assignment

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-let addtodobutton = document.getElementById('addtodo');
-let todocontainer = document.getElementById('todocontainer');
-let inputfield = document.getElementById('inputfield');
-let btnClearlist = document.querySelector('button');
-let paragraph = document.querySelectorAll('p');
+document.querySelector('#push').onclick = function(){
+  if(document.querySelector('#newtask input').value.length == 0){
+      alert("Kindly Enter Task Name!!!!")
+  }
 
-addtodobutton.addEventListener('click', function(){
-    var paragraph = document.createElement('p')
-    paragraph.classList.add('paragraph-styling')
-    paragraph.innerText = inputfield.value;
-    todocontainer.appendChild(paragraph);
-    inputfield.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    btnClearlist.addEventListener('click', () => {
-        paragraph.forEach(input => input.value = '');
-    });
+  else{
+      document.querySelector('#tasks').innerHTML += `
+          <div class="task">
+              <span id="taskname">
+              
+                  ${document.querySelector('#newtask input').value}
+              </span>
+              <button class="delete">
+                  <i class="far fa-trash-alt"></i>
+              </button>
+          </div>
+      `;
 
-});
-
-
+      var current_tasks = document.querySelectorAll(".delete");
+      for(var i=0; i<current_tasks.length; i++){
+          current_tasks[i].onclick = function(){
+              this.parentNode.remove();
+          }
+      }
+  }
+}
 
